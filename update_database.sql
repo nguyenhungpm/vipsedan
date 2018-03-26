2e6a5e4eb5077b2549f654eb6f7eec0c0db8cf6f
Nguyễn Hùng <nguyenhung537@gmail.com>
2018-03-26 08:21:07 +0700

    them bang booking

---
+++ update_database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `se_booking` (
+  `booking_id` int(11) NOT NULL,
+  `schedule` varchar(255) NOT NULL,
+  `note` text NOT NULL,
+  `office_id` int(11) NOT NULL DEFAULT '0',
+  `money` int(11) NOT NULL DEFAULT '0',
+  `discount` int(11) NOT NULL DEFAULT '0',
+  `state_receive` tinyint(1) NOT NULL DEFAULT '1',
+  `fee_ticket` int(11) NOT NULL DEFAULT '0',
+  `fee_fuel` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_execute` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `se_booking`
+  ADD PRIMARY KEY (`booking_id`);
+
+ALTER TABLE `se_booking`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
